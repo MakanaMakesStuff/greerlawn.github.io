2cccb2001b1aefc765f2c199c3b034effbd686e2
MakanaMakesStuff <37227196+MakanaMakesStuff@users.noreply.github.com>
2018-06-08 19:35:44 -0400

    Greer Update #1

---
+++ routes/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Greer Lawn Services</title>
+    <link rel="stylesheet" href="../styles/index.css">
+</head>
+<body>
+    <header></header>
+    <section></section>
+    <footer></footer>
+</body>
+</html>
